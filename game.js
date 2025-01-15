99f4de9c8afab79496448b4daca3626711f4715d
freethrownucleus <80530013+freethrownucleus@users.noreply.github.com>
2025-01-16 00:56:44 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let username, password;
 let loginFlag = false, gameInProgress = false;
 let mainGame;
-let url = "http://projekt-diplomski-igra-nim.com/";
+let url = "https://projekt-diplomski-igra-nim.com/";
 let divsArray = ["homePageDiv", "gameFormDiv", "gameDiv", "restartGameDiv", "leaveGameDiv", "rankingDiv", "rulesDiv"];
 
 let homePageDiv = document.getElementById("homePageDiv");
